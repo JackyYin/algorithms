9a56ba473d25bc8e88f45149d54eb90586e28e4c
JackyYin <jjyyg1123@gmail.com>
2021-01-13 10:06:39 +0800

    feat: Sqrt(x), 100, 100

---
+++ leetcode/69.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 1;
+        int right = x / 2;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (mid > x / mid) {
+                right = mid - 1;
+            }   else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+};
