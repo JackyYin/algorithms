041ff3b4b7d7a2cbf7abec92156837b92284be8e
殷豪 <yinhao@yinhaodeMacBook-Pro.local>
2021-05-11 00:25:29 +0800

    feat: Count Primes, 45.72, 38.64
    - Sieve of Eratosthenes

---
+++ leetcode/204.c
@@ -0,0 +1,26 @@
+#include "stdbool.h"
+
+int countPrimes(int n){
+	if (!n) return 0;
+	bool isPrime[n];
+
+	for (int i = 1; i < n; i++) {
+		isPrime[i] = true;
+	}
+
+	for (int i = 2; i * i < n; i++) {
+		if (!isPrime[i]) continue;
+
+		for (int j = i * i; j < n; j += i) {
+			isPrime[j] = false;
+		}
+	}
+
+
+	int cnt = 0;
+	for (int i = 2; i < n; i++) {
+		if (isPrime[i]) cnt++;
+	}
+
+	return cnt;
+}
