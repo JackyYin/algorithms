8af64c30c2599fea01d0ded30f9f93ea22adca8a
Jacky_Yin <jacky_yin@trendmicro.com>
2021-05-28 13:46:48 +0800

    feat: Partitioning Into Minimum Number Of Deci-Binary Numbers, 46.81, 72.16

---
+++ 1689.cpp
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int minPartitions(string n) {
+        int max = 0;
+        for (char c : n) {
+            if (c - '0' > max) max = c - '0';
+        }
+        
+        return max;
+    }
+};
